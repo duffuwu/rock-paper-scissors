8e5e2de51066fcf820a841205abf0feba5748413
duffuwu <jvitorsa@gmail.com>
2022-05-11 18:49:05 -0300

    Added second function for the game. This function evaluates who the winner is given two inputs.

--- script.js
+++ script.js
@@ -2,6 +2,26 @@ function ComputerPlay() {
     let randomNumber = Math.floor(
         Math.random() * 3
         )
-    let wordsArray = ['Rock','Paper','Scissors']
+    let wordsArray = ['rock','paper','scissors']
     return wordsArray[randomNumber]
+}
+
+function singleRound(playerSelection,computerSelection) {
+    let player = playerSelection.toLowerCase()
+
+    if (playerSelection === computerSelection) {
+        return `Draw! Both picked ${playerSelection}`
+    }
+
+    else if (
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper')
+    ) {
+        return `You win! ${playerSelection} beats ${computerSelection}`
+    }
+
+    else {
+        return `You lose! ${playerSelection} beats ${computerSelection}`
+    }
 }
