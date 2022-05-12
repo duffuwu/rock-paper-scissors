282349d388704f1ba3fa6531bb858a559a440e77
duffuwu <jvitorsa@gmail.com>
2022-05-11 21:03:29 -0300

    Added a function to execute the game and reordered the previous functions to work with it. The game now runs five times, tells at the end of each round who won and the current score. At the end of the five round, the game displays who wins.

--- script.js
+++ script.js
@@ -1,27 +1,61 @@
-function ComputerPlay() {
-    let randomNumber = Math.floor(
-        Math.random() * 3
-        )
-    let wordsArray = ['rock','paper','scissors']
-    return wordsArray[randomNumber]
-}
+function game() {
 
-function singleRound(playerSelection,computerSelection) {
-    let player = playerSelection.toLowerCase()
+    let computerScore = 0,
+        playerScore = 0
 
-    if (playerSelection === computerSelection) {
-        return `Draw! Both picked ${playerSelection}`
+    function playRound() {
+
+        let playerSelection = prompt("Choose between rock, paper or scissors").toLowerCase();
+
+        function computerPlay() {
+            let randomNumber = Math.floor(
+                Math.random() * 3
+                );
+            let wordsArray = ['rock','paper','scissors'];
+            return wordsArray[randomNumber];
+
+            }
+
+        let computerPick = computerPlay()
+
+        if (playerSelection === computerPick) {
+            return `Draw! Both picked ${playerSelection}`;
+        }
+
+        else if (
+            (playerSelection === 'paper' && computerPick === 'rock') ||
+            (playerSelection === 'rock' && computerPick === 'scissors') ||
+            (playerSelection === 'scissors' && computerPick === 'paper')
+        ) {
+            playerScore++;
+            return `You win! ${playerSelection} beats ${computerPick} \n 
+            Player: ${playerScore} \n
+            Computer: ${computerScore}`;
+        }
+
+        else {
+            computerScore++;
+            return `You lose! ${computerPick} beats ${playerSelection} \n
+            Player: ${playerScore} \n
+            Computer: ${computerScore}`;
+        }
     }
 
-    else if (
-        (playerSelection === 'paper' && computerSelection === 'rock') ||
-        (playerSelection === 'rock' && computerSelection === 'scissors') ||
-        (playerSelection === 'scissors' && computerSelection === 'paper')
-    ) {
-        return `You win! ${playerSelection} beats ${computerSelection}`
+    for (let i = 0; i <= 5; i++) {
+        console.log(playRound())
+    }
+
+    if (playerScore === computerScore) {
+        console.log("Final Result: DRAW!");
+    }
+
+    else if (playerScore > computerScore) {
+        console.log("Final Result: PLAYER WINS")
     }
 
     else {
-        return `You lose! ${playerSelection} beats ${computerSelection}`
+        console.log("Final Result: COMPUTER WINS")
     }
 }
+
+game()
