441db986a7b79fb41bfc0965d494b1ab03ce3554
duffuwu <jvitorsa@gmail.com>
2022-06-19 12:30:43 -0300

    Created the index with its features and respective classes. The main content was positioned in the general area they are going to be.

--- script.js
+++ script.js
@@ -1,61 +1,40 @@
-function game() {
+let computerScore = 0,
+    playerScore = 0
 
-    let computerScore = 0,
-        playerScore = 0
+function playRound() {
 
-    function playRound() {
+    let playerSelection = prompt("Choose between rock, paper or scissors").toLowerCase();
 
-        let playerSelection = prompt("Choose between rock, paper or scissors").toLowerCase();
+    function computerPlay() {
+        let randomNumber = Math.floor(
+            Math.random() * 3
+            );
+        let wordsArray = ['rock','paper','scissors'];
+        return wordsArray[randomNumber];
 
-        function computerPlay() {
-            let randomNumber = Math.floor(
-                Math.random() * 3
-                );
-            let wordsArray = ['rock','paper','scissors'];
-            return wordsArray[randomNumber];
-
-            }
-
-        let computerPick = computerPlay()
-
-        if (playerSelection === computerPick) {
-            return `Draw! Both picked ${playerSelection}`;
         }
 
-        else if (
-            (playerSelection === 'paper' && computerPick === 'rock') ||
-            (playerSelection === 'rock' && computerPick === 'scissors') ||
-            (playerSelection === 'scissors' && computerPick === 'paper')
-        ) {
-            playerScore++;
-            return `You win! ${playerSelection} beats ${computerPick} \n 
-            Player: ${playerScore} \n
-            Computer: ${computerScore}`;
-        }
+    let computerPick = computerPlay()
 
-        else {
-            computerScore++;
-            return `You lose! ${computerPick} beats ${playerSelection} \n
-            Player: ${playerScore} \n
-            Computer: ${computerScore}`;
-        }
+    if (playerSelection === computerPick) {
+        return `Draw! Both picked ${playerSelection}`;
     }
 
-    for (let i = 0; i <= 5; i++) {
-        console.log(playRound())
-    }
-
-    if (playerScore === computerScore) {
-        console.log("Final Result: DRAW!");
-    }
-
-    else if (playerScore > computerScore) {
-        console.log("Final Result: PLAYER WINS")
+    else if (
+        (playerSelection === 'paper' && computerPick === 'rock') ||
+        (playerSelection === 'rock' && computerPick === 'scissors') ||
+        (playerSelection === 'scissors' && computerPick === 'paper')
+    ) {
+        playerScore++;
+        return `You win! ${playerSelection} beats ${computerPick} \n 
+        Player: ${playerScore} \n
+        Computer: ${computerScore}`;
     }
 
     else {
-        console.log("Final Result: COMPUTER WINS")
+        computerScore++;
+        return `You lose! ${computerPick} beats ${playerSelection} \n
+        Player: ${playerScore} \n
+        Computer: ${computerScore}`;
     }
 }
-
-game()
